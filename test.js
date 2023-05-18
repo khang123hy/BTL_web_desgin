c29d654810a4975abca915e29478b662f982e674
khang123hy <duykhang02vnn@gmail.com>
2023-05-18 09:42:02 +0700

    update 18/5

--- test.js
+++
@@ -1,6 +0,0 @@
-$('.has-dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).fadeIn('fast');
-  }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).fadeOut('fast');
-  });
-  
